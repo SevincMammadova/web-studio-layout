65f3f84d4587ad0f58f0d4872ad895347299d29a
SevincMammadova <sevincmammadova609@gmail.com>
2022-11-16 19:16:31 +0400

    start

---
+++ src/blocks/modules/home-intro/home-intro.js
@@ -0,0 +1,13 @@
+modules.define('home-intro', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
